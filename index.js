96264930e7c669fd0cb47b3630904e3e7197af03
tacchf <tacchf@amazon.com>
2019-10-09 11:15:16 +0200

    PI calculation using Pythagoras

---
+++ index.js
@@ -0,0 +1,36 @@
+const yargs = require('yargs');
+
+const argv = yargs.option("iterations", {
+        alias: "i",
+        description: "Number of iterations to run the aproximation for",
+        type: "number"
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+if(argv.iterations && argv.iterations > 0) {
+    const n = argv.iterations; 
+    const aprox = calculatPolygonSideAmount(n) * calculatePolygonSideWidth(n);
+
+    console.log("The value of PI is: " + aprox);
+    return aprox;
+}
+
+function calculatPolygonSideAmount(n) {
+    return Math.pow(2, n+1);
+}
+
+
+function calculatePolygonSideWidth(n) {
+    if(n == 1 || n < 1) {
+        return Math.sqrt(0.5)
+    }
+
+    const previousApproximation = calculatePolygonSideWidth(n - 1)
+
+    const width = previousApproximation/2;
+    const height = (1 - Math.sqrt(1 - Math.pow(previousApproximation, 2)))/2;
+
+    return Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+}
